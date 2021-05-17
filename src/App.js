dfdaed1edcc0128cdf3121782a113e6ef2812838
souravchiku <76004164+souravchiku@users.noreply.github.com>
2021-05-17 20:42:17 +0530

    adding completed functionality hidding and showing completed functionality

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import "./styles.css";
 export default function App() {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
+  const [completed, setCompleted] = useState([]);
+  const [bool, setBool] = useState(false);
 
   useEffect(() => {
     let json = localStorage.getItem("todos");
+    let json2 = localStorage.getItem("completed");
+    let completed2 = JSON.parse(json2);
     let loadedTodos = JSON.parse(json);
-    if (loadedTodos) {
+    if (loadedTodos || completed2) {
       setTodos(loadedTodos);
+      setCompleted(completed2);
+    }
+    if (completed2) {
+      setBool(true);
     }
   }, []);
 
   useEffect(() => {
     let json = JSON.stringify(todos);
+    let json2 = JSON.stringify(completed);
+    localStorage.setItem("completed", json2);
     localStorage.setItem("todos", json);
   }, [todos]);
 
@@ -33,6 +43,33 @@ export default function App() {
     e.preventDefault();
     setTodos([]);
     setTodo("");
+    setCompleted([]);
+    setBool(false);
+  }
+
+  function completedButton(id, item) {
+    // setCompleted([...completed].concat(item))
+    console.log(item);
+
+    let updatedlist = [...todos].map((item) => {
+      if (item.id === id) {
+        item.completed = true;
+      }
+    });
+    setTodos(updatedlist);
+    console.log(item);
+  }
+
+  function handleradio(item) {
+    setBool(true);
+    let key = item.id;
+    console.log("helo handle", item, key, todos);
+    setCompleted([...completed].concat(item));
+    console.log(completed);
+    let updatedtodos = todos.filter((item) => item.id !== key);
+
+    console.log(updatedtodos);
+    setTodos(updatedtodos);
   }
 
   return (
@@ -72,11 +109,15 @@ export default function App() {
       >
         {todos.map((item) => {
           return (
-            <div className="todo-text">
+            <div className="todo-text" key={item.id}>
               {/* {console.log(item.completed)} */}
               <p>
                 <label>
-                  <input name="group1" type="radio" />
+                  <input
+                    name="group1"
+                    type="radio"
+                    onChange={() => handleradio(item)}
+                  />
                   <span>{item.text}</span>
                 </label>
               </p>
@@ -84,7 +125,25 @@ export default function App() {
           );
         })}
       </div>
-      <div></div>
+      {bool ? (
+        <div style={{ borderTop: "1px solid grey" }}>
+          <h5 id="completed">Completed</h5>
+          {completed.map((val) => {
+            return (
+              <div className="todo-text" key={val.id}>
+                <p>
+                  <label>
+                    <input type="checkbox" checked="true" />
+                    <span>{val.text}</span>
+                  </label>
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
